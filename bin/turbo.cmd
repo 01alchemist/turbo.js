91a1b5c33a7155219d8203ce42e7c65a71971608
Nidin Vinayakan <01@01alchemist.com>
2016-07-18 20:30:39 +0200

    v 1.0.3 bin modified

--- bin/turbo.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\turbo.js\bin\tc" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\turbo.js\bin\tc" %*
-)
