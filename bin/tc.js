e8365f5f30e6cdae41fe64c9f0aa345c475b13cf
Nidin Vinayakan <01@01alchemist.com>
2016-07-19 01:35:39 +0200

    v 1.0.3 bin modified

--- bin/tc.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by r3f on 4/7/2016.
- */
-var Compiler = require("./../src/modules/parallel.js/compiler/Compiler").Compiler;
-
-var compiler = new Compiler();
-compiler.compile(process.argv.slice(2));
